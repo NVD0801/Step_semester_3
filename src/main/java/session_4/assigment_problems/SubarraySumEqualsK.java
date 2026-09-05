e71fece8083faa9437707634f85edb6edcc5a65f
Naveed <naveed99690@gmail.com>
2026-09-05 16:38:50 +0530

    Update SubarraySumEqualsK.java

---
+++ src/main/java/session_4/assigment_problems/SubarraySumEqualsK.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int currentSum = 0;
+        Map<Integer, Integer> prefixSumMap = new HashMap<>();
+        prefixSumMap.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            if (prefixSumMap.containsKey(currentSum - k)) {
+                count += prefixSumMap.get(currentSum - k);
+            }
+
+            prefixSumMap.put(currentSum, prefixSumMap.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
