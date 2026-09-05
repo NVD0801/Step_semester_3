31c2731f8dcf8cbd2127cb9f22c7e9cad5115a23
Naveed <naveed99690@gmail.com>
2026-09-05 10:52:21 +0530

    Add files via upload

---
+++ src/main/java/session_4/class_problems/RotateArray.java
@@ -0,0 +1,13 @@
+public class RotateArray {
+    public int[] rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[(i + k) % n] = nums[i];
+        }
+
+        return result;
+    }
+}
